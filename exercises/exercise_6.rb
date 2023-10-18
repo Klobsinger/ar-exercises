30aff2cfd29b0c423a047c6301a2ba0d13cb0ac0
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-10-18 15:19:47 -0400

    table relations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kevin", last_name: "Lobo", hourly_rate: 60000)
+@store1.employees.create(first_name: "joe", last_name: "joeson", hourly_rate: 6)
+@store1.employees.create(first_name: "leo", last_name: "wantstuna", hourly_rate: 20)
+@store1.employees.create(first_name: "robin", last_name: "cuddle", hourly_rate: 5)
+
+@store2.employees.create(first_name: "dog", last_name: "dogyson", hourly_rate: 20)
+@store2.employees.create(first_name: "cat", last_name: "catyson", hourly_rate: 50)
+@store2.employees.create(first_name: "baghoul", last_name: "isreal", hourly_rate: 100)
+@store2.employees.create(first_name: "shadow", last_name: "hedgehogison", hourly_rate: 2060)
