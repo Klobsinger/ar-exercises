94a34ad1e36b342e9512be83c802a76a6332e9b2
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-10-18 15:36:17 -0400

    validation and error logging

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "enter store name"
+store_name = gets.chomp
+
+new_store = Store.new(name:store_name)
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Failed to create the store due to the following errors:"
+  new_store.errors.full_messages.each do |error_message|
+    puts error_message
+  end
+end
