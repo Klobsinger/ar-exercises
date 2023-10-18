28db862c28c20c69d8432584bd53a74b9b7f598e
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-10-18 15:09:30 -0400

    Sum Average & stacking methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_annual_revenue = Store.sum(:annual_revenue)
+average_annual_revenue = Store.average(:annual_revenue)
+over_1m = Store.where("annual_revenue < 1000000").count
+
+puts "Total annual revenue for all stores: #{total_annual_revenue}"
+puts "Average annual revenue for all stores: #{average_annual_revenue}"
+puts "over one milla billa: #{over_1m}"
